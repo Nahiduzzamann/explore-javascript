99ebf9e7b5210ac2f5b551cdea466984291c9f5b
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-01-27 03:34:21 +0600

    ok

---
+++ math.js
@@ -0,0 +1,30 @@
+//numeric
+var price = 11;
+var age = 22;
+var temperature = 39;
+//string
+var name = "md. nahiduzzaman";
+
+//boolenan
+ var ok = true;
+ var isSingle =  false;
+
+
+ //variable name
+
+ console.log(price - age);
+
+ //division
+
+ var money = 500;
+ var people = 10;
+ //var eachPeople = 500 / 10 ;
+ var eachPeople = 10 / 500 ;
+
+ //variable we can update any time
+
+ var priceOfApple = 100;
+//set new value
+ priceOfApple = 150;
+
+ 
