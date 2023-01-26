20fefae0acdcb8be2619dc091bf24d517efbbcf1
Md. Nahiduzzaman <nahid.cse.diu.19@gmail.com>
2023-01-27 04:16:26 +0600

    ok

--- math.js
+++ math.js
@@ -12,7 +12,7 @@ var name = "md. nahiduzzaman";
 
  //variable name
 
- console.log(price - age);
+ //console.log(price - age);
 
  //division
 
@@ -29,9 +29,37 @@ var name = "md. nahiduzzaman";
 
 
  //string concatenation 
-  var firstName = 'Md';
+  var firstName = 'Md ';
   var secondName = 'Nahiduzzaman';
-var fullName = firstName + ' ' +secondName;
-console.log(fullName);
+//var fullName = firstName + secondName;
+//var fullName = firstName + ' ' +secondName;
+
+//console.log(fullName);
 
  
+//convert string to number
+
+var id1 = '23432';
+var idNumber = parseInt(id1);
+
+
+
+//console.log(typeof id1);
+
+var num1 = 0.1;
+var num2 = 0.33;
+var sum = num1 + num2 ;
+//console.log(sum)
+sum= sum.toFixed(3);
+//console.log(sum)
+sum=parseFloat(sum)
+//console.log(sum)
+
+
+
+//remainder
+
+var mango = 7;
+var person =3;
+var remainder = mango % person ;
+//console.log(remainder)
